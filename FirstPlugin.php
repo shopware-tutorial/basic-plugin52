c7b037107a2614328ad0f07c45c348248f98dd6c
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2017-04-09 11:23:14 +0200

    03. Shopware 5 | Controller Events - PreDispatch und PostDispatchSecure [DE]

--- FirstPlugin.php
+++ FirstPlugin.php
@@ -4,7 +4,73 @@ namespace FirstPlugin;
 
 use Shopware\Components\Plugin;
 
+/**
+ * Class FirstPlugin
+ * @package FirstPlugin
+ */
 class FirstPlugin extends Plugin
 {
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'Enlight_Controller_Action_PreDispatch_Frontend_Detail' => 'redirectToLoginWhenNot18',
+            'Enlight_Controller_Action_PostDispatchSecure_Frontend_Detail' => 'changeTitle'
+        ];
+    }
+
+    /**
+     * @param \Enlight_Controller_ActionEventArgs $args
+     */
+    public function redirectToLoginWhenNot18(\Enlight_Controller_ActionEventArgs $args)
+    {
+        $articleId = (int)$args->getRequest()->getParam('sArticle');
+        $catId = $this->getCatId();
+        $articleInCat = (bool)Shopware()->Db()->fetchOne(
+            'SELECT id FROM s_articles_categories WHERE articleID = ? AND categoryID = ?',
+            [
+                $articleId, $catId
+            ]
+        );
+        if ($articleInCat && !Shopware()->Modules()->Admin()->sCheckUser()) {
+            return $args->getSubject()->redirect('/register');
+        }
+    }
+
+    /**
+     * @param \Enlight_Controller_ActionEventArgs $args
+     */
+    public function changeTitle(\Enlight_Controller_ActionEventArgs $args)
+    {
+        $view = $args->getSubject()->View();
+        $sArticle = $view->getAssign('sArticle');
+        if ($sArticle['categoryID'] === $this->getCatId()) {
+            $sArticle['articleName'] = $sArticle['articleName'] . '(' . $this->getTransletionByIdent('local_product') . ')';
+            $view->assign('sArticle', $sArticle);
+        }
+    }
+
+    /**
+     * @return int
+     */
+    private function getCatId(): int
+    {
+        return (int)Shopware()->Config()->getByNamespace('FirstPlugin', 'catId');
+    }
+
+    /**
+     * @param string $ident
+     * @return string
+     */
+    private function getTransletionByIdent(string $ident): string
+    {
+        /** @var \Enlight_Components_Snippet_Namespace $namespace */
+        $namespace = $this->container->get('snippets')->getNamespace(
+            'frontend/detail/firstplugin'
+        );
+        return $namespace->get($ident, 'Regionalprodukt', true);
+    }
 
 }
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 #### 02. Shopware 5 | Basic Plugin-Info [DE]
 
 [![02. Shopware 5 | Basic Plugin-Info [DE]](https://i.ytimg.com/vi/j9PoAUt0V6w/hqdefault.jpg)](https://www.youtube.com/watch?v=j9PoAUt0V6w "02. Shopware 5 | Basic Plugin-Info [DE]")
+
+#### 03. Shopware 5 | Controller Events - PreDispatch und PostDispatchSecure [DE]
+
+[![03. Shopware 5 | Controller Events - PreDispatch und PostDispatchSecure [DE]](https://i.ytimg.com/vi/e7o-EhlawHQ/hqdefault.jpg)](https://youtu.be/e7o-EhlawHQ "03. Shopware 5 | Controller Events - PreDispatch und PostDispatchSecure [DE]")
